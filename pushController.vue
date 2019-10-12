913d733949ccb5f74110ab82df4ac5a599b51af5
sunil <sunil@logos.social>
2019-10-12 12:36:34 +0530

    added setup for fcm notifications for andriod

--- pushController.vue
+++ pushController.vue
@@ -7,11 +7,12 @@ import PushNotification from 'react-native-push-notification';
 export default {
   name: 'push-controller',
   created: function () {
-    console.log("calling========");
     PushNotification.configure({
      onNotification: function(notification) {
       console.log("NOTIFICATION:", notification);
-     }
+     },
+    //  requried for remote notifications
+    //  senderId: 'your gcm or fcm senderId generally it is 12 digit number'
     });
   }
 }
